65b786fe6ee8554bef1a6267cb92d3e556df3002
Cadis Hangoluan <cadishangoluan95@gmail.com>
2025-06-27 07:19:20 +0700

    optimize: remove unused file

--- utility/src/commonMain/kotlin/com/utility/DateTime.kt
+++
@@ -1,7 +0,0 @@
-package com.utility
-
-import kotlinx.datetime.LocalDateTime
-
-expect fun String.asDateTime(pattern: String): LocalDateTime?
-
-expect fun LocalDateTime.asString(pattern: String): String?
